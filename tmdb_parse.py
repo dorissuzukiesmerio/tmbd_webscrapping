5abebbe71d260e7edd581cdbc17ba29814532b74
dorissuzukiesmerio <dsuzuki@g.clemson.edu>
2021-10-14 17:58:03 -0400

    parsing json : getting movie info for one movie

--- tmdb_parse.py
+++ tmdb_parse.py
@@ -6,4 +6,16 @@ import os # do folder in operating system
 if not os.path.exists("parsed_files"):
 	os.mkdir("parsed_files")
 
+df = pandas.DataFrame()
 
+json_file_name = "json_files/tmdb550.json"
+f = open(json_file_name, "r")
+json_data = json.load(f)
+f.close()
+
+# print(json_data)
+
+# Nested structure:
+
+print(json_data['adult'])
+print(json_data['backdrop_path'])
--- tmdb_request.py
+++ tmdb_request.py
@@ -14,13 +14,14 @@ f.close()
 if not os.path.exists("json_files"):
 	os.mkdir("json_files")
 
-response = urllib.request.urlopen("https://api.themoviedb.org/3/movie/?api_key=" + api_key)
+response = urllib.request.urlopen("https://api.themoviedb.org/3/movie/?api_key="+api_key)
 json_response = json.load(response)
 movie_end = int(json_response['id']) 
 
 movie_start = movie_end - 10
 
 for movie_id in range(movie_start, movie_end):
+	print(movie_id)
 	# movie_id = 550 # used only for one (while seeing if the code is working, before putting for loop for all)
 	response = urllib.request.urlopen("https://api.themoviedb.org/3/movie/" + str(movie_id) + "?api_key=" + api_key)
 
